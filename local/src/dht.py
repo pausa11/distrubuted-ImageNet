2eca363f507e333d1cf8af9709c7d9897f03c372
Daniel Toro Soto <daniel.toro1@utp.edu.co>
2025-10-29 22:51:10 -0500

    eliminando archivos incesarios

--- local/src/dht.py
+++
@@ -1,4 +0,0 @@
-import hivemind
-dht = hivemind.DHT(start=True, host_maddrs=['/ip4/127.0.0.1/tcp/4000'], initial_peers=[])
-print("/ip4/127.0.0.1/tcp/4000")
-input("Bootstrap DHT corriendo. Enter para salir.\n")
--- local/src/model.py
+++
@@ -1,9 +0,0 @@
-import torch
-import torch.nn as nn
-import torchvision.models as models
-
-def resnet50_tiny(num_classes=200):
-    m = models.resnet50(weights=None)
-    # Tiny-ImageNet tiene 200 clases
-    m.fc = nn.Linear(m.fc.in_features, num_classes)
-    return m
